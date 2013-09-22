f079d8465ccda2163a8e32ae974daa615192d70e
Arley Rodrigues <arleysb@gmail.com>
2013-09-22 02:40:19 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" lang="pt_BR" />
+    <title>Sistema de Estoque</title>
+  </head>
+  
+  <body>
+  
+    Corpo da Página
+    
+  </body>
+</html>
